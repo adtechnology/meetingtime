28ec7b1c64e7d9b8877246c8de3688f8b16483f2
Carlos Bracho <carlos.bracho@axelspringer.com>
2024-07-19 23:50:35 +0200

    added caching service worker dark mode improved

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'meetingtime-cache-v1';
+const urlsToCache = [
+    '/',
+    '/styles.min.css',
+    '/script.min.js',
+    '/fonts/Poppins-Regular.ttf',
+    '/fonts/Poppins-Bold.ttf'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
